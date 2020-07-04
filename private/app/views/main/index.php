02a0da453214e9a185f2e2a0ef5aa641316dc2a6
meghar <megha.ratakonda500@gmail.com>
2020-07-04 04:18:00 +0000

    commit2

--- private/app/views/main/index.php
+++ private/app/views/main/index.php
@@ -1,4 +1,83 @@
-<h1>Main Index</h1>
+<!--<h1>Main Index</h1>
 <p>This is the main page that is also the default.</p>
 
 <a href="//www.google.ca">Google</a>
+-->
+
+
+
+
+<!--top starts-->
+<body class="image">
+	<div id="mainheading">
+<h1>My Blog !!</h1>
+</div>
+   <nav id="nav">
+    <ul>
+        <li><a  href="temp.html"><span>Home</span></a></li>
+        <li> <a href="listing.html" ><span>Blog Listing</span></a></li>
+        <li><a  href="singleblog.html"><span>View Single Blog</span></a></li>
+    </ul>
+</nav>
+
+<!--top ends-->
+
+
+<!--middle starts-->
+
+<div id="top">
+            <h2><strong>PHP Blog !!!</strong></h2>
+</div>
+
+
+		
+		<div id="subheading">
+		<h2>All you need to know !!</h2>
+			</div>
+		
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+			
+		</p>
+
+		<div id="subheading">
+			<h2>All you need to know !!</h2>
+				</div>
+			
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+				
+			</p>
+
+			<div id="subheading">
+				<h2>All you need to know !!</h2>
+					</div>
+				
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odit, perspiciatis libero mollitia magnam, facere architecto aliquam nemo tenetur et eligendi soluta ullam aperiam quo dignissimos doloribus fugiat, impedit ipsa.
+					
+				</p>
+	
+
+<!--middle ends-->
+
+
+
+
+
+</body>
+
+
+</html>
