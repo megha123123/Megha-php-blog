1ed895b47fbbb710f062ca3fc04fcb7d4503f88f
meghar <megha.ratakonda500@gmail.com>
2020-07-30 17:56:59 +0000

    commit

--- setup/data/init.sql
+++ setup/data/init.sql
@@ -5,11 +5,19 @@ email varchar(255) primary key
 , password_hash varchar(255) not null
 );
 
-CREATE TABLE blog_posts (
-    slug varchar(40) primary key
+CREATE TABLE blog_lists (
+    serial_num int primary key
     , post_name varchar(255)
     , post_content text
     , author_email varchar(255) REFERENCES author(email) 
     , publish_date DATETIME 
 
 ); 
+
+INSERT INTO author values("kierra.gentry@gmail.com","kierra","password");
+INSERT INTO author values("miranda.shaffer@gmail.com","miranda","password");
+INSERT INTO author values("thomas.crane@gmail.com","thomas","password");
+
+INSERT INTO blog_lists values("1","kierra","This is the content","kierra.gentry@gmail.com",curdate());
+INSERT INTO blog_lists values("2","miranda","This is the content","miranda.shaffer@gmail.com",curdate());
+INSERT INTO blog_lists values("3","thomas","This is the content","thomas.crane@gmail.com",curdate());
