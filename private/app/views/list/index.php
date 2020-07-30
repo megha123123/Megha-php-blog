c4152cb75e6563e4f6886a7c90fa680a5d9d094a
meghar <megha.ratakonda500@gmail.com>
2020-07-30 21:47:33 +0000

    commit

--- private/app/views/list/index.php
+++ private/app/views/list/index.php
@@ -3,23 +3,29 @@
    <a href="/main/single/<?php echo $serial_num;?>"> 
 
    <?php
-echo $blog_name;
-    ?></a>
+        echo $blog_name;
+    ?>
+    </a>
     </h2>
-
 <h4>
      <?php
-echo $author_email;
+        echo $author_name;
+    ?>
+    </h4>
+    
+<h4>
+     <?php
+        echo $author_email;
     ?>
     </h4>
 
 
 <h4><?php
-echo $publish_date;
+        echo $publish_date;
     ?></h4>
 
 <p><?php
-echo $blog_content;
+        echo $blog_content;
     ?></p>
 
 
