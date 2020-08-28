a699fb18418f053d04e60f6dc267c1d8c88b0f46
meghar <megha.ratakonda500@gmail.com>
2020-08-28 05:14:09 +0000

    commit

--- private/app/controllers/main.php
+++ private/app/controllers/main.php
@@ -175,11 +175,8 @@ function login () {
         
          session_start();
 
-// Unset all of the session variables.
 $_SESSION = array();
 
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -188,7 +185,6 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Finally, destroy the session.
 session_destroy();
         header("Location: /main/Index");        
 
